4a752b305dddb468a9b53e0acf9020e155f2259b
Miguel Hallux <miguelhallux@gmail.com>
2024-10-02 11:30:05 +0100

    Create bd_games.sql

---
+++ bd_games.sql
@@ -0,0 +1,19 @@
+ENTIDADES (Tabelas) -- sem acentos e letra pequena 
+
+- jogos
+- generos
+- produtoras
+- usuarios
+
+
+
+ATRIBUTOS  das ENTIDADES
+
+utilizadores -- não tem Relacionamento com entidade jogos
+* 
+* 
+* 
+
+
+
+jogos
