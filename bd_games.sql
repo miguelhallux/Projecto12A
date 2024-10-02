e8f030aeb93ee9826f2e10d6e673cdbba26df80e
Miguel Hallux <miguelhallux@gmail.com>
2024-10-02 12:38:53 +0100

    Update bd_games.sql

--- bd_games.sql
+++ bd_games.sql
@@ -1,19 +1,7 @@
-ENTIDADES (Tabelas) -- sem acentos e letra pequena 
-
-- jogos
-- generos
-- produtoras
-- usuarios
+-VAMOS CRIAR NA QUERY
 
 
+-- Cria a base de dados 'bd_games'
 
-ATRIBUTOS  das ENTIDADES
-
-utilizadores -- não tem Relacionamento com entidade jogos
-* 
-* 
-* 
-
-
-
-jogos
+CREATE DATABASE db_games DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- banco de dados será criado com o conjunto de caracteres  padrão UTF-8 , garantindo suporte a uma ampla gama de caracteres latinos, e com a collation (ordenação) UTF-8 case-insensitive, que é insensível a maiúsculas e minúsculas.
