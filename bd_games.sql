02666d42d35484aa6e28b8a2d557b76f212d4c2c
Miguel Hallux <miguelhallux@gmail.com>
2024-10-02 12:52:14 +0100

    Update bd_games.sql

--- bd_games.sql
+++ bd_games.sql
@@ -5,3 +5,12 @@
 
 CREATE DATABASE db_games DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 -- banco de dados será criado com o conjunto de caracteres  padrão UTF-8 , garantindo suporte a uma ampla gama de caracteres latinos, e com a collation (ordenação) UTF-8 case-insensitive, que é insensível a maiúsculas e minúsculas.
+
+
+CREATE TABLE utilizadores(
+  
+    utilizador VARCHAR(10) NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    pass VARCHAR(60) NOT NULL,
+    tipo VARCHAR(10) NOT NULL DEFAULT 'editor',
+    PRIMARY KEY (utilizador));
