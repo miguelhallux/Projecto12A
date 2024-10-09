e0e74e050b8d82b181c6560b84d630a7c1f00886
Miguel Hallux <miguelhallux@gmail.com>
2024-10-09 11:02:45 +0100

    Update bd_games.sql

--- bd_games.sql
+++ bd_games.sql
@@ -8,9 +8,44 @@ CREATE DATABASE db_games DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
 CREATE TABLE utilizadores(
-  
+   
     utilizador VARCHAR(10) NOT NULL,
     nome VARCHAR(30) NOT NULL,
     pass VARCHAR(60) NOT NULL,
     tipo VARCHAR(10) NOT NULL DEFAULT 'editor',
-    PRIMARY KEY (utilizador));
+    PRIMARY KEY (utilizador)
+
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+
+-- Criação da tabela 'generos'
+CREATE TABLE generos(
+    cod int(11) not null,           
+    genero varchar(20) not null,   
+    PRIMARY KEY(cod)                
+) ENGINE=INNODB DEFAULT charset=utf8;              
+
+
+-- Criação da tabela 'produtoras'
+CREATE TABLE produtoras(
+    cod int(11) not null,           
+    produtora varchar(20) not null, 
+    pais varchar(15) not null,      
+    PRIMARY KEY(cod)                
+) ENGINE=INNODB DEFAULT charset=utf8;               
+
+-- Criação da tabela 'jogos'
+CREATE TABLE jogos(
+    cod int(11) not null,              
+    nome varchar(40) not null,        
+    cod_genero int(11) not null,       
+    cod_produtora int(11) not null,    
+    descricao text,                   
+    pontuacao decimal(4,2) not null, 
+    capa varchar(40) default null,    
+    PRIMARY KEY(cod),                
+    FOREIGN KEY(cod_genero) REFERENCES generos(cod),         
+    FOREIGN KEY(cod_produtora) REFERENCES produtoras(cod)     
+) 
+ENGINE=INNODB DEFAULT charset=utf8;    
