9d70b481cf62ed74869852b1ebaf5df6e48e9b71
Miguel Hallux <miguelhallux@gmail.com>
2024-10-09 12:24:29 +0100

    Create apontamentos.sql

---
+++ apontamentos.sql
@@ -0,0 +1,7 @@
+
+
+-- 3 Bibliotecas para acessar a base de dados no PHP
+
+mysql_* -- esta totalmente defasada, do 5.1 para a frente ján se usa
+mysqli -- é o mais rapido e orientado ao objecto (i de improved) so funciona com SQL
+PDO -- PHP DATA OBJECT (100% Orientada ao objecto) ainda esta em evolução , da suporte a 12 SGBD
